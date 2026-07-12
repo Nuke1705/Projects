2c8cf0689b806a87c18373efe0af516a6d038e6f
Nuke1705 <debrajdasiitg27@gmail.com>
2026-07-13 02:47:33 +0530

    Update gravity.cpp

--- N-Body Gravity Simulation/gravity.cpp
+++ N-Body Gravity Simulation/gravity.cpp
@@ -15,7 +15,7 @@
 #include "res\imgui\imgui_impl_glfw.h"
 #include "res\imgui\imgui_impl_opengl3.h"
 
-#include "Simulation\simulation.h"
+#include "Simulation2\simulation.h"
 
 #include <iostream>
 #include <string>
@@ -28,69 +28,62 @@ using namespace Engine;
 //-----------------------------------------------------------------------------------
 #define WIDTH 1920.0f
 #define HEIGHT 1080.0f
-#define ParticleCount 20000
-#define timestep 0.1f
+#define ParticleCount 100000
+#define timestep 0.01f
+#define M 50000.0
 //-----------------------------------------------------------------------------------
 
 vector<float> ParticleObjectPoints(vector<Vec2>& positions){
 
-    float sign[4] = {0.0f, 0.0f, 1.0f, 1.0f};
-    vector<float> result(8*ParticleCount,0);
+    vector<float> result;
+    result.reserve(2*ParticleCount);
 
     for(int i = 0; i < ParticleCount; ++i){
-        for(int j = 0; j < 4; ++j){
-            result[2*j + 8*i] = positions[i].x + (2*sign[(j+1)%4]-1);
-            result[2*j + 1 + 8*i] = positions[i].y + (2*sign[j]-1);
-        }
-
+        result.emplace_back(positions[i].x);
+        result.emplace_back(positions[i].y);
     }
     return result;
 }
-// 0 1 2 3
-// - + + -
-// - - + +
-
-vector<unsigned int> ParticleIndices(){
-
-    unsigned int reference[6] = {
-        0, 1, 2,
-        2, 3, 0
-    };
-    vector<unsigned int> result(6*ParticleCount,0);
-
-    for(int i = 0; i < ParticleCount; ++i){
-        for(int j = 0; j < 6; ++j){
-            result[6*i + j] = 4*i + reference[j];
-        }
+void generateMass(vector<float>& arr){
+    arr.emplace_back(M);
+    arr.emplace_back(M);
+    for(int i = 0; i < ParticleCount-2; ++i){
+        arr.emplace_back(1.0);
     }
-    return result;
 }
 //-----------------------------------------------------------------------------------
+vector<float> mass;
 bool paused = false;
+vector<float> particles;
+float zoom = 1;
+
 int main(void){
+    float scale = 1.0;
     Window window(WIDTH , HEIGHT, "N-Body");
-    Simulation gravity({WIDTH, HEIGHT}, ParticleCount, timestep);
+    Simulation gravity({2*scale*WIDTH, 2*scale*HEIGHT}, {-scale*WIDTH, -scale*HEIGHT}, ParticleCount, timestep);
 
     {
     
-    vector<Vec2> original_position = gravity.generate_positions();
-    vector<Vec2> Velocities(ParticleCount, {0,0});
-    vector<vector<Vec2>> Config = {original_position, Velocities};
-    vector<unsigned int> indices = ParticleIndices();
+    //vector<Vec2> original_position = gravity.generate_positions();
+    //vector<Vec2> Velocities(ParticleCount, {0,0});
+    //vector<vector<Vec2>> Config = {original_position, Velocities};
+    vector<vector<Vec2>> original_Config = gravity.generateGalaxy({-500.0, 10.0}, {500, 50.0}, M, M);
+    vector<vector<Vec2>> Config = original_Config;
+    generateMass(mass);
+    gravity.initialise(Config, mass);
 //-----------------------------------------------------------------------------------
-    VertexBuffer vb(nullptr, 8*ParticleCount*sizeof(float), true);
+    VertexBuffer vb(nullptr, 2*ParticleCount*sizeof(float), true);
     VertexBufferLayout layout;
     layout.Push(GL_FLOAT, 2);
     VertexArray va;
     va.AddBuffer(vb, layout);
 
-    IndexBuffer ib(indices.data(), 6*ParticleCount);
-
-    mat4 proj = ortho(0.0f, WIDTH, 0.0f, HEIGHT, -1.0f, 1.0f);
+    //mat4 proj = ortho(0.0f, WIDTH, 0.0f, HEIGHT, -1.0f, 1.0f);
+    mat4 proj;
     mat4 view = translate(mat4(1.0f), vec3(0,0,0));
     mat4 model = translate(mat4(1.0f), vec3(0,0,0));
-
-    mat4 mvp = proj * view * model;
+    mat4 temp = view * model;
+    mat4 mvp;
 
     Shader shader("res/shaders/simple.shader");
 
@@ -99,8 +92,8 @@ int main(void){
     shader.Unbind();
     //vb.Unbind();
     //va.Unbind();
-    ib.Unbind();
-
+    //ib.Unbind();
+    
 //-----------------------------------------------------------------------------------
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
@@ -125,33 +118,31 @@ int main(void){
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 //-----------------------------------------------------------------------------------        
-        //glDrawArrays(GL_TRIANGLES, 0, 6);
-        vector<float> particles = ParticleObjectPoints(Config[0]);
+        particles = ParticleObjectPoints(Config[0]);
+        proj = ortho(-WIDTH/(zoom), WIDTH/(zoom), -HEIGHT/(zoom), HEIGHT/(zoom), -1.0f, 1.0f);
+        mvp = proj * temp;
         shader.Bind();
-        //shader.SetUniform1f("u_time", renderer.GetTime());
         shader.SetUniformMat4f("u_MVP", mvp);
         
         vb.Update(particles.data(), particles.size()*sizeof(float));
-        renderer.Draw(va, ib, shader);
+        renderer.DrawP(va, ParticleCount, shader);
         if(!paused){
-            Config = gravity.NewState(Config, nodeCount);
+            Config = gravity.NewState(Config, mass, nodeCount);
         }   
 //-----------------------------------------------------------------------------------
        {
 
             ImGui::Begin("Simulation Data");                          // Create a window called "Hello, world!" and append into it.
 
-            //ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-
-            //ImGui::SameLine();
             ImGui::Text("Particle Count = %d", ParticleCount);
             if (ImGui::Button("Pause")){                            // Buttons return true when clicked (most widgets return true when edited/activated)
                 paused = !paused;
             }
             ImGui::SameLine();
             if (ImGui::Button("Reset")){                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                Config = {original_position, Velocities};
+                Config = original_Config;//{original_position, Velocities};
             }
+            ImGui::SliderFloat("zoom", &zoom, 0.0f, 4.0f);
             ImGui::Text("Time step = %f", timestep);
             ImGui::Text("Number of nodes = %d", nodeCount);
             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
