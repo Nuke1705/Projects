f3ec140b5f262e4ce3e841e56e03a108f7830f63
Nuke1705 <debrajdasiitg27@gmail.com>
2026-07-13 02:48:40 +0530

    Update simulation.h

--- N-Body Gravity Simulation/Simulation/simulation.h
+++ N-Body Gravity Simulation/Simulation/simulation.h
@@ -12,19 +12,29 @@ class QuadNodePool;
 class Simulation{
     private:
         const Vec2 windowDimensions;
+        const Vec2 corner;
         const long long N;
         const float timestep;
-        const float MIN_SIZE = 5.0f;
+        const float MIN_SIZE = 0.0f;
+        const float softening = 25.0f;
+        const float theta_squared = 0.75;
+        vector<Vec2> acc;
+        vector<Vec2> new_pos;
+        vector<Vec2> new_vel;
+        //QuadNodePool NodePool;
     public:
-        Simulation(const Vec2& Dimensions, long long ParticleCount, float timestep);
+        Simulation(const Vec2& Dimensions, const Vec2& corner, long long ParticleCount, float timestep);
         ~Simulation();
         vector<Vec2> generate_positions();
-        vector<vector<Vec2>> NewState(vector<vector<Vec2>>& Config, unsigned int& out);
+        vector<vector<Vec2>> generateGalaxy(Vec2 core1, Vec2 core2, float M1, float M2);
+        void initialise(vector<vector<Vec2>>& Config, vector<float>& mass);
+        vector<vector<Vec2>> NewState(vector<vector<Vec2>>& Config, vector<float>& mass, unsigned int& out);
 
     private:
         Vec2 com(Vec2& pos1, float m1, Vec2& pos2, float m2);
-        unsigned int build_tree(Vec2& coord, QuadNode* node, QuadNodePool& nodes);
+        unsigned int build_tree(Vec2& coord, QuadNode* node, QuadNodePool& nodes, float m);
         Vec2 GetForce(Vec2& coord, QuadNode* node);
-        Vec2 UpdatePosition(Vec2& coord, Vec2& Force, Vec2& Velocity);
-        Vec2 UpdateVelocity(Vec2& Force, Vec2& Velocity, Vec2& coord);
+        Vec2 getForce(Vec2& coord, QuadNode* node);
+        void UpdatePosition(Vec2& coord, Vec2& Force, Vec2& Velocity, Vec2& target);
+        void UpdateVelocity(Vec2& Force, Vec2& Velocity, Vec2& target);
 };
